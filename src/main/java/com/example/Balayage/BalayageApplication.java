7318dce07fce8fbf9ecf1cf6c99da431a674d474
Ali-Doggaz <charfedine611@gmail.com>
2021-07-01 12:38:59 +0100

    Init Spring Boot Project

---
+++ src/main/java/com/example/Balayage/BalayageApplication.java
@@ -0,0 +1,13 @@
+package com.example.Balayage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalayageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BalayageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Balayage/BalayageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Balayage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BalayageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
