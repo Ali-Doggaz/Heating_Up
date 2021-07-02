19417111e0781a302afd8efdcadc34dd405e004d
Ali-Doggaz <charfedine611@gmail.com>
2021-07-02 11:37:04 +0100

    removed useless comments

--- src/main/java/com/example/Balayage/regles/TestRegles.java
+++ src/main/java/com/example/Balayage/regles/TestRegles.java
@@ -39,21 +39,3 @@ public class TestRegles {
     }
 
 }
-
-//Boolean message = (Boolean) parser.parseExpression("'Hello World'.concat('!')").getValue();
-//Integer val = parser.parseExpression("16 * 5").getValue(Integer.class);
-// System.out.println("Result of 16*5 = " + val);
-//boolean boolVal = parser.parseExpression("5 < 9").getValue(Boolean.class);
-//System.out.println("4. Mathematical operator value:\n" + boolVal);
-// 5. Logical operator
-//boolVal = parser.parseExpression("400 > 200 && 200 < 500").getValue(Boolean.class);
-//System.out.println("5. Logical operator value:\n" + boolVal);
-
-// 6. Ternary operator
-//String strVal = parser.parseExpression("'some value' != null ? 'some value' : 'default'").getValue(String.class);
-//System.out.println("6. Ternary operator value:\n" + strVal);
-
-
-// 8. Regex/matches operator
-//boolVal = parser.parseExpression("'UPPERCASE STRING' matches '[A-Z\\s]+'").getValue(Boolean.class);
-//System.out.println("8. Regex/matches operator value:\n" + boolVal);
