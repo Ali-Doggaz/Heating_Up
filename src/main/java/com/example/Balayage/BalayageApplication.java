1539f89df6cdd23d479312cec02d892eeb28deed
Ali-Doggaz <charfedine611@gmail.com>
2021-07-06 11:22:56 +0100

    Changed settings to debug mode

--- src/main/java/com/example/Balayage/BalayageApplication.java
+++ src/main/java/com/example/Balayage/BalayageApplication.java
@@ -33,7 +33,7 @@ public class BalayageApplication {
 	//TODO modifier ca (la ca s'active toutes les 1 minutes)
 	//Genere un rapport tous les jours, à 8heures
 
-	@Scheduled(cron = "0 * 8 * * *")
+	@Scheduled(cron = "0 */1 * * * *")
 	public void schedule() throws JobParametersInvalidException, JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException {
 		{
 			jobLauncher.run(scanJob, new JobParametersBuilder()
