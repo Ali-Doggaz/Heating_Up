947ef94cb1a3ddd681594f16214c90e163254875
Ali-Doggaz <charfedine611@gmail.com>
2021-10-02 12:54:44 +0100

    first commit

--- src/main/java/com/example/Balayage/regles/clientsTestResults/ClientTestResultRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.Balayage.regles.clientsTestResults;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClientTestResultRepository  extends JpaRepository<ClientTestResult, Long> {
-}
