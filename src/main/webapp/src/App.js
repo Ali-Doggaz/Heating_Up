857d8e9141bf6093fcef53a5b76ba42b1d0254a9
dalideco <dalissj1@gmail.com>
2021-10-03 10:43:59 +0100

    adding transition

--- src/main/webapp/src/App.js
+++ src/main/webapp/src/App.js
@@ -4,6 +4,8 @@ import { Route, Switch } from 'react-router';
 import HomePage from './pages/HomePage';
 import { useHistory } from 'react-router';
 import Search from './pages/Search';
+import TransitionScreen from './components/TransitionScreen';
+import { CSSTransition } from 'react-transition-group';
 
 const routes = [
   { path:"/search",Component:Search, name:'search'},
@@ -22,15 +24,30 @@ function App() {
   return (
     <div className="App">
       <div className="background"></div>
-      <Switch>
+     
+      
       {
         routes.map(({path,Component,name})=>(
-          <Route path={path} key={name}>
-            <Component/>
+          <Route path={path} exact key={name}>
+            {({match})=>(
+              <CSSTransition
+                in={match != null}
+                timeout={1300}
+                classNames="page"
+                unmountOnExit
+              >
+                <div className="page">
+                  <TransitionScreen/>
+                  <Component/>
+                </div>
+              </CSSTransition>
+            )}
+            
           </Route>
         ))
       }
-      </Switch>
+      
+      
     </div>
   );
 }
